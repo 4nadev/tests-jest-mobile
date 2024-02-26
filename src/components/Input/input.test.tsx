055b5bf4b91505f34ed2f1855f7615cd1820075b
Aninha <anamarianevesbatista@gmail.com>
2024-02-26 13:37:03 -0300

    🧪 test: Test changes

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,8 +4,14 @@ import { shouldUseActivityState } from 'react-native-screens';
 
 describe('Componet: Input', () => {
     it('Should be render without activity indicator', () => {
-        render(<Input  />);
+        render(<Input />);
         const ActivityIndicator = screen.queryByTestId('activity-indicator');
         expect(ActivityIndicator).toBeNull();
+    });
+
+    it('Should be render without activity indicator if isLoading prop is true', () => {
+        render(<Input isLoading />);
+        const ActivityIndicator = screen.getByTestId('activity-indicator');
+        expect(ActivityIndicator).toBeTruthy();
     });
 });
