ec3e49b47ca99a8068f3f75f5113433dba47eb3c
Aninha <anamarianevesbatista@gmail.com>
2024-02-26 10:52:00 -0300

    🧪 test: testID

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 import { Input } from '@components/Input';
 import { shouldUseActivityState } from 'react-native-screens';
 
 describe('Componet: Input', () => {
 	it('Should be render without activity indicator', () => {
-		const { debug } = render(<Input />);
-		debug();
+	 render(<Input isLoading />);
+	const ActivityIndicator = screen.getByTestId('activity-indicator');
+    console.log(ActivityIndicator)
 	});
 });
